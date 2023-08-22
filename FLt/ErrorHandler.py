b35a8c8a0310455afbe2505b20181507f4ce97d1
victor.yang <victor.yang@veoneer.com>
2023-08-22 22:39:39 +0800

    0822

--- FLt/ErrorHandler.py
+++ FLt/ErrorHandler.py
@@ -1,19 +1,22 @@
+import traceback
 def divide_numbers(a, b):
-    try:
+    # result = a / b
+    # try:
         result = a / b
-        print("结果：", result)
-    except ZeroDivisionError:
-        print("除数不能为零！")
-    except TypeError:
-        print("输入的数据类型不正确！")
-    except Exception as e:
-        print("发生了一个未知错误：", str(e))
+    #     print("result：", result)
+    # except Exception:
+    #     traceback.print_exc()
 
-# 测试错误处理程序
-divide_numbers(10, 2)   # 正常情况，输出结果：5.0
+divide_numbers(10, 2)
+divide_numbers(10, 0)
+divide_numbers(10, '2')
+divide_numbers(10, [])   #  unsupported operand type(s) for /: 'int' and 'list'
 
-divide_numbers(10, 0)   # 除以零，捕获 ZeroDivisionError，输出结果：除数不能为零！
-
-divide_numbers(10, '2')  # 类型错误，捕获 TypeError，输出结果：输入的数据类型不正确！
-
-divide_numbers(10, [])   # 发生未知错误，捕获 Exception，输出结果：发生了一个未知错误： unsupported operand type(s) for /: 'int' and 'list'
+#     print("ZeroDivisionError！")
+#     print(traceback.format_exc())
+# except TypeError:
+#     print("TypeError！")
+#     print(traceback.format_exc())
+# except Exception as e:
+#     print("Unknow error：", str(e))
+#     print(traceback.format_exc())
