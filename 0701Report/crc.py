1e9b54c21f1af52b46449741f600a1faf30d11f1
victoryang126 <44468953+victoryang126@users.noreply.github.com>
2023-08-22 23:16:59 +0800

    0820

--- 0701Report/crc.py
+++ 0701Report/crc.py
@@ -16,24 +16,45 @@ class CRC:
         return crc_table
 
     @staticmethod
-    def calculate_direct_reflected_crc(data, poly, initial_value=0xFFFFFFFF, final_xor=0xFFFFFFFF, width=32):
+    def calculate_crc(data, polynomial, initial_value=0xFFFFFFFF, final_xor=0xFFFFFFFF, width=32, input_reflect=False, result_reflect=False):
         crc = initial_value
+
         for byte in data:
-            crc ^= byte
+            if input_reflect:
+                byte = CRC.reverse_bits(byte, 8)
+
+            crc ^= byte << (width - 8)
             for _ in range(8):
-                if crc & 1:
-                    crc = (crc >> 1) ^ poly
+                if crc & (1 << (width - 1)):
+                    crc = (crc << 1) ^ polynomial
                 else:
-                    crc >>= 1
-        print(hex(crc ^ final_xor))
-        return crc ^ final_xor
+                    crc <<= 1
+            crc &= (1 << width) - 1
+
+        if result_reflect:
+            crc = CRC.reverse_bits(crc, width)
+        crc = crc ^ final_xor
+        print(hex(crc))
+        return crc
 
     @staticmethod
-    def calculate_crc_with_table(data, crc_table, initial_value=0xFFFFFFFF, final_xor=0xFFFFFFFF, width=32):
-        crc = initial_value
+    def calculate_crc_with_table(data, crc_table, crc_initial, final_xor, width=32, input_reflect=False, result_reflect=False):
+        crc = crc_initial
+
         for byte in data:
-            crc = (crc >> 8) ^ crc_table[(crc ^ byte) & 0xFF]
-        return crc ^ final_xor
+            if input_reflect:
+                byte = CRC.reverse_bits(byte, 8)
+
+            index = (crc ^ byte) & 0xFF
+            crc = (crc >> 8) ^ crc_table[index]
+            # crc =  crc_table[index]
+            crc &= ((1 << width) - 1)
+
+        if result_reflect:
+            crc = CRC.reverse_bits(crc, width)
+        crc = (crc ^ final_xor) & ((1 << width) - 1)
+        print(hex(crc))
+        return crc
 
     @staticmethod
     def reverse_bits(data, width):
@@ -52,12 +73,13 @@ if __name__ == "__main__":
     reversed_data = [CRC.reverse_bits(byte, 8) for byte in data]
 
     # CRC-32 parameters
-    poly = 0x1021
-    crc_table = CRC.generate_crc_table(poly, width=16)
+    poly = 0x1D
+    crc_table = CRC.generate_crc_table(poly, width=8)
 
 
-    CRC.calculate_direct_reflected_crc(bytearray([0x31,0x32,0x33]),0x1D,0xFF,0xFF,8)
-    # CRC.calculate_direct_reflected_crc(bytearray([0x31, 0x32, 0x33]),0x1021, 0xFF, 0xFF, 8)
+    CRC.calculate_crc(bytearray([0x31,0x32,0x33]),0x1D,0xFF,0xFF,8)
+    CRC.calculate_crc_with_table(bytearray([0x31,0x32,0x33]),crc_table,0x1D,0xFF,0xFF,8)
+    CRC.calculate_crc(bytearray([0x31, 0x32, 0x33]),0x1021, 0x0, 0x0, 16)
     # # Calculate CRC-32 using table
     # crc32_result = CRC.calculate_crc_with_table(reversed_data, crc32_table, width=32)
     #
