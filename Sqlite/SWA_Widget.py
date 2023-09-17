64de9e11d62b046e81721648125d8d0fb5ea0ee5
victoryang126 <44468953+victoryang126@users.noreply.github.com>
2023-09-17 12:36:17 +0800

    091702

--- Sqlite/SWA_Widget.py
+++ Sqlite/SWA_Widget.py
@@ -125,34 +125,67 @@ class SWA_Widget(QWidget):
     #         printer_dc.TextOut(100, 100, table)
     #         printer_dc.EndPage()
     #         printer_dc.EndDoc()
+    def print_item_to_printer(self, table_content):
+        printer_dialog = QPrintDialog()
 
-    def print_item_to_printer(self, table):
-        printer = QPrinter(QPrinter.HighResolution)
-        printer.setOutputFormat(QPrinter.PdfFormat)
-        printer.setOutputFileName('output.pdf')  # 指定输出的 PDF 文件名
+        if printer_dialog.exec_() == QPrintDialog.Accepted:
+            printer = printer_dialog.printer()
+            document = QTextDocument()
+            cursor = QTextCursor(document)
+            cursor.setCharFormat(QTextCharFormat())
 
-        dialog = QPrintDialog(printer, self)
-        if dialog.exec_() == QPrintDialog.Accepted:
-            painter = QPainter()
-            painter.begin(printer)
+            # 创建表格
+            table = cursor.insertTable(len(table_content), len(table_content[0]))
+            table_format = table.format()
+            table_format.setHeaderRowCount(1)  # 如果需要标题行，请设置为1
 
-            # 设置绘制的位置和样式
-            x, y = 100, 100
-            column_width = 100
-            row_height = 30
-            font = QFont("Arial", 12)
+            for row, row_data in enumerate(table_content):
+                for col, cell_data in enumerate(row_data):
+                    cell = table.cellAt(row, col)
+                    cell_cursor = cell.firstCursorPosition()
+                    cell_cursor.insertText(str(cell_data))
 
-            # 开始绘制表格
-            painter.setFont(font)
-            for row_data in table:
-                x = 100
-                for item in row_data:
-                    painter.drawRect(x, y, column_width, row_height)
-                    painter.drawText(x + 5, y + 20, column_width - 10, row_height - 10, Qt.AlignLeft, str(item))
-                    x += column_width
-                y += row_height
+            # 调整表格样式和布局
+            table_format.setAlignment(Qt.AlignLeft)
+            table_format.setBorderStyle(QTextFrameFormat.BorderStyle_Solid)
+            table_format.setWidth(QTextLength(QTextLength.PercentageLength, 100))  # 表格宽度自适应
 
-            painter.end()
+            # 将表格添加到文档中
+            cursor.movePosition(QTextCursor.End)
+            cursor.insertBlock()
+            cursor.insertTable(table_format)
+
+            # 打印文档
+            printer.setDocName('Inventory')
+            printer.setResolution(300)  # 设置打印分辨率
+            document.print_(printer)
+    # def print_item_to_printer(self, table):
+    #     printer = QPrinter(QPrinter.HighResolution)
+    #     printer.setOutputFormat(QPrinter.PdfFormat)
+    #     printer.setOutputFileName('output.pdf')  # 指定输出的 PDF 文件名
+    #
+    #     dialog = QPrintDialog(printer, self)
+    #     if dialog.exec_() == QPrintDialog.Accepted:
+    #         painter = QPainter()
+    #         painter.begin(printer)
+    #
+    #         # 设置绘制的位置和样式
+    #         x, y = 100, 100
+    #         column_width = 100
+    #         row_height = 30
+    #         font = QFont("Arial", 12)
+    #
+    #         # 开始绘制表格
+    #         painter.setFont(font)
+    #         for row_data in table:
+    #             x = 100
+    #             for item in row_data:
+    #                 painter.drawRect(x, y, column_width, row_height)
+    #                 painter.drawText(x + 5, y + 20, column_width - 10, row_height - 10, Qt.AlignLeft, str(item))
+    #                 x += column_width
+    #             y += row_height
+    #
+    #         painter.end()
     # def print_item_to_printer(self, table):
     #     printer = QPrinter(QPrinter.HighResolution)
     #
