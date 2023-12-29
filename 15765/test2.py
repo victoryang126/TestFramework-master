d7065bc6af4406eff4648d24cafb05003735d44f
victoryang126 <44468953+victoryang126@users.noreply.github.com>
2023-12-29 20:50:05 +0800

    1107

--- 15765/test2.py
+++ 15765/test2.py
@@ -1,19 +1,105 @@
-from datetime import datetime
+import struct
+from typing import Tuple, List
 
-def convert_asc_date_to_datetime(asc_date_str):
-    try:
-        # You may need to adjust the format based on the actual date format in your ASC log
-        datetime_obj = datetime.strptime(asc_date_str, "%a %b %d %I:%M:%S %p %Y")
-        return datetime_obj
-    except ValueError as e:
-        print(f"Error converting ASC date to datetime: {e}")
-        return None
+class FileHeaderStruct:
+    def __init__(self, data):
+        self.signature, \
+        self.header_size, \
+        self.app_id, \
+        self.app_major, \
+        self.app_minor, \
+        self.app_build, \
+        self.bin_log_major, \
+        self.bin_log_minor, \
+        self.bin_log_build, \
+        self.bin_log_patch, \
+        self.file_size, \
+        self.uncompressed_size, \
+        self.obj_count, \
+        self.obj_count_read, \
+        *self.app_version, \
+        *self.bin_log_version = struct.unpack("<4sLBBBBBBBBQQLL8H8H", data)
 
-if __name__ == "__main__":
-    asc_date_str = "Wed Aug 09 01:27:06 AM 2023"  # Replace with the actual ASC date string
-    datetime_obj = convert_asc_date_to_datetime(asc_date_str)
+class ObjHeaderBaseStruct:
+    def __init__(self, data):
+        self.signature, \
+        self.header_size, \
+        self.header_version, \
+        self.obj_size, \
+        self.obj_type = struct.unpack("<4sHHLL", data)
 
-    if datetime_obj:
-        print("Converted Datetime:", datetime_obj)
-    else:
-        print("Conversion failed.")
+class ObjHeaderV1Struct:
+    def __init__(self, data):
+        self.flags, \
+        self.client_index, \
+        self.obj_version, \
+        self.timestamp = struct.unpack("<LHHQ", data)
+
+class ObjHeaderV2Struct:
+    def __init__(self, data):
+        self.flags, \
+        self.timestamp_status, \
+        self.obj_version, \
+        self.timestamp = struct.unpack("<LBxHQ8x", data)
+
+class LogContainerStruct:
+    def __init__(self, data):
+        self.compression_method, \
+        self.size_uncompressed = struct.unpack("<H6xL4x", data)
+
+class CanMsgStruct:
+    def __init__(self, data):
+        self.channel, \
+        self.flags, \
+        self.dlc, \
+        self.arbitration_id, \
+        self.data = struct.unpack("<HBBL8s", data)
+
+class CanFdMsgStruct:
+    def __init__(self, data):
+        self.channel, \
+        self.flags, \
+        self.dlc, \
+        self.arbitration_id, \
+        _, \
+        _, \
+        self.fd_flags, \
+        self.valid_bytes, \
+        self.data = struct.unpack("<HBBLLBBB5x64s", data)
+
+class CanFdMsg64Struct:
+    def __init__(self, data):
+        self.channel, \
+        self.dlc, \
+        self.valid_bytes, \
+        _, \
+        self.arbitration_id, \
+        _, \
+        self.fd_flags, \
+        _, \
+        _, \
+        _, \
+        _, \
+        _, \
+        self.direction, \
+        _, \
+        _, = struct.unpack("<BBBBLLLLLLLHBBL", data)
+
+class CanErrorExtStruct:
+    def __init__(self, data):
+        self.channel, \
+        self.dlc, \
+        self.valid_payload_length, \
+        _, \
+        self.arbitration_id, \
+        _, \
+        self.fd_flags, \
+        _, \
+        _, \
+        _, \
+        _, \
+        _, \
+        _, \
+        self.ecc, \
+        self.position, \
+        self.data = struct.unpack("<HHLBBBxLLH2x8s", data)
